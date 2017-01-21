f004030aa8173804bdab1720a0bafe728b57d7a2
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-01-21 17:41:32 +0100

    refactor tests - place data closer to the test that uses it

--- test/bowling/core_test.clj
+++ test/bowling/core_test.clj
@@ -2,14 +2,13 @@
   (:use midje.sweet)
   (:use [bowling.core]))
 
-(let [a-gutter-game [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
-      an-all-ones-game [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1]]
-
-  (facts "about bowling score"
+(facts "about bowling score"
+       (let [a-gutter-game [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]]
          (fact "it scores a gutter game"
-               (score a-gutter-game) => 0)
+               (score a-gutter-game) => 0))
+       (let [an-all-ones-game [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1]]
          (fact "it scores an all ones game"
-               (score an-all-ones-game) => 20)
-         (let [a-game-with-a-spare [5 5 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]]
-           (fact "it scores a game with a spare"
-                 (score a-game-with-a-spare) => 16))))
+               (score an-all-ones-game) => 20))
+       (let [a-game-with-a-spare [5 5 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]]
+         (fact "it scores a game with a spare"
+               (score a-game-with-a-spare) => 16)))
