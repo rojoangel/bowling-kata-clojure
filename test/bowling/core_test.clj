e21b5d4d42364b2d9ae99adeac362c363b4e9b5f
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-01-21 17:18:52 +0100

    refactor - extract a gutter game

--- test/bowling/core_test.clj
+++ test/bowling/core_test.clj
@@ -2,7 +2,7 @@
   (:use midje.sweet)
   (:use [bowling.core]))
 
-
-(facts "about bowling score"
-  (fact "it scores a game with all zeroes"
-        (score [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]) => 0))
+(let [a-gutter-game [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]]
+  (facts "about bowling score"
+         (fact "it scores a gutter game"
+               (score a-gutter-game) => 0)))
