5efd7a0a9884374a8820e7e5bb408456b40bbf40
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-01-21 17:54:38 +0100

    it scores a game with a strike

--- src/bowling/core.clj
+++ src/bowling/core.clj
@@ -3,7 +3,12 @@
 (defn spare? [roll1 roll2]
   (= 10 (+ roll1 roll2)))
 
+(defn strike? [roll]
+  (= 10 roll))
+
 (defn score [rolls]
-  (if (spare? (first rolls) (second rolls))
-    (+ (apply + (take 3 rolls)) (score (drop 2 rolls)))
-    (apply + rolls)))
+  (if (strike? (first rolls))
+    (+ (apply + (take 3 rolls)) (score (rest rolls)))
+    (if (spare? (first rolls) (second rolls))
+      (+ (apply + (take 3 rolls)) (score (drop 2 rolls)))
+      (apply + rolls))))
--- test/bowling/core_test.clj
+++ test/bowling/core_test.clj
@@ -11,4 +11,7 @@
                (score an-all-ones-game) => 20))
        (let [a-game-with-a-spare [5 5 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]]
          (fact "it scores a game with a spare"
-               (score a-game-with-a-spare) => 16)))
+               (score a-game-with-a-spare) => 16))
+       (let [a-game-with-a-strike [10 3 4 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]]
+         (fact "it scores a game with a strike"
+               (score a-game-with-a-strike) => 24)))
