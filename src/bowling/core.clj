c5c1fb8596514e3d4070863cb7817d89b39eb5e8
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-01-21 21:22:44 +0100

    rename strike-bonus? and spare-bonus? and change them to receive all the remaining rolls

--- src/bowling/core.clj
+++ src/bowling/core.clj
@@ -6,23 +6,23 @@
 (defn- next-frame-strike? [rolls]
   (= 10 (first rolls)))
 
-(defn- strike-bonus? [rolls]
-  (= 2 (count rolls)))
+(defn- game-ends-with-strike? [rolls]
+  (= 2 (count (rest rolls))))
 
-(defn- spare-bonus? [rolls]
-  (= 1 (count rolls)))
+(defn- game-ends-with-spare? [rolls]
+  (= 1 (count (drop 2 rolls))))
 
 (defn score [rolls]
   (if (empty? rolls)
     0
     (if (next-frame-strike? rolls)
       (+ (apply + (take 3 rolls))
-         (if (strike-bonus? (rest rolls))
+         (if (game-ends-with-strike? rolls)
            (score (drop 2 (rest rolls)))
            (score (rest rolls))))
       (if (next-frame-spare? rolls)
         (+ (apply + (take 3 rolls))
-           (if (spare-bonus? (drop 2 rolls))
+           (if (game-ends-with-spare? rolls)
              (score (drop 1 (drop 2 rolls)))
              (score (drop 2 rolls))))
         (+ (apply + (take 2 rolls)) (score (drop 2 rolls)))))))
